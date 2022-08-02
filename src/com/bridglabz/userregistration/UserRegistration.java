1cd59099cd0c24c7d1735b7d16af6fab07333179
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-02 23:09:03 +0530

    Done with last name

--- src/com/bridglabz/userregistration/UserRegistration.java
+++ src/com/bridglabz/userregistration/UserRegistration.java
@@ -1,4 +1,28 @@
 package com.bridglabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+    public void validLastName() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Last name : ");
+        String lastName = sc.nextLine();
+
+        boolean lName = Pattern.matches("^[A-Z]+[a-z]{3,}$", lastName);
+
+        if (lName) {
+            System.out.println("lastname is correct ");
+        } else {
+            System.out.println("lastname is incorrect ");
+        }
+
+    }
+        public static void main (String[]args){
+            UserRegistration userRegistration = new UserRegistration();
+            userRegistration.validLastName();
+        }
+
 }
