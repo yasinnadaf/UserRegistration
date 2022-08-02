1105456d0cf99cd41992f154752ea662ff167df2
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-02 21:42:28 +0530

    Modified: UC1 Done valid first name

--- src/com/bridglabz/userregistration/UserRegistration.java
+++ src/com/bridglabz/userregistration/UserRegistration.java
@@ -1,4 +1,29 @@
 package com.bridglabz.userregistration;
 
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class UserRegistration {
+
+    public void validFirstName() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First name : ");
+        String firstName = sc.nextLine();
+
+        boolean fName = Pattern.matches("^[A-Z]+[a-z]{3,}$", firstName);
+        System.out.println(firstName);
+
+        if (fName) {
+            System.out.println("firstname is correct ");
+        } else {
+            System.out.println("firstname is incorrect ");
+        }
+    }
+
+     public static void main(String[] args) {
+         UserRegistration userRegistration = new UserRegistration();
+         userRegistration.validFirstName();
+     }
+
 }
