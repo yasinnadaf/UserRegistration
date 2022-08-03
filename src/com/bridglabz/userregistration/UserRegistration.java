dff5d417bce38015455e9d482d0887d81d4113fc
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-03 22:27:35 +0530

    valid phone number

--- src/com/bridglabz/userregistration/UserRegistration.java
+++ src/com/bridglabz/userregistration/UserRegistration.java
@@ -1,4 +1,26 @@
 package com.bridglabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public void validPhoneNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Phone number: ");
+        String phoneNumber = sc.nextLine();
+        boolean number = Pattern.matches("^[9][1][ ][6-9][0-9]{9}$",phoneNumber);
+
+        if(number){
+            System.out.println("Phone number is correct");
+        }
+        else{
+            System.out.println("Phone number is incorrect");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        UserRegistration getNumber = new UserRegistration();
+        getNumber.validPhoneNumber();
+    }
 }
