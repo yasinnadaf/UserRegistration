8357ee5858c05cad2397fc9bd54a75e33c9dd971
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-03 21:37:07 +0530

    Done checking valid email

--- src/com/bridglabz/userregistration/UserRegistration.java
+++ src/com/bridglabz/userregistration/UserRegistration.java
@@ -1,4 +1,26 @@
 package com.bridglabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+    public void validEmailId() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter email id");
+        String email = sc.nextLine();
+
+        boolean validEmail = Pattern.matches("^[a-z]*.[a-z]+@[a-z]+.[a-z]{2,3}(.[a-z]{2,})*$", email);
+        if (validEmail) {
+            System.out.println("Email is valid");
+        } else {
+            System.out.println("Email is invalid");
+        }
+    }
+
+    public static void main(String[] args) {
+        UserRegistration mail = new UserRegistration();
+        mail.validEmailId();
+
+    }
 }
