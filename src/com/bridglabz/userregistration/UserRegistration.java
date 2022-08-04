2917b3f5c1a6ef865f299e4ca05735165ddacf20
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-04 21:12:33 +0530

    mini\]

--- src/com/bridglabz/userregistration/UserRegistration.java
+++ src/com/bridglabz/userregistration/UserRegistration.java
@@ -1,4 +1,27 @@
 package com.bridglabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+    public static void main(String[] args) {
+        UserRegistration check = new UserRegistration();
+        check.validPassword();
+    }
+
+
+    public static void validPassword(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter user password");
+        String password = sc.nextLine();
+        boolean validatePassword = Pattern.matches("^[a-z]{8,}$",password);
+
+        if(validatePassword){
+            System.out.println("password is valid");
+        }
+        else{
+            System.out.println("passward is invalid");
+        }
+    }
 }
